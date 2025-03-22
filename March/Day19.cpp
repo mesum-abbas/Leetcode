4c1f9d5b337e3b699e6a23c2f92d0f7f48c771f0
mesum-abbas <messum321@gmail.com>
2025-03-22 21:04:51 +0500

    Create Day19.cpp

---
+++ March/Day19.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    void rotate90(vector<vector<int>>& mat , int n){
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                swap(mat[i][j],mat[j][i]);
+            }
+        }
+        for(auto &row : mat) {
+            reverse(begin(row), end(row));
+        }
+    }
+    bool checkequal(vector<vector<int>>& mat, vector<vector<int>>& target ,int n){
+        for(int i=0;i<n;i++){
+            if(mat[i]!=target[i])
+            return false;
+        }
+            return true;
+    }
+
+    bool findRotation(vector<vector<int>>& mat, vector<vector<int>>& target) {
+        int n = mat.size();
+        for(int i=0;i<4;i++){
+            rotate90(mat,n);
+            if(checkequal(mat,target,n))
+                return true;
+        }
+        return false;
+    }
+};
+
+
+
+
+
+
+
