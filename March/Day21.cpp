14114ac3aa225eca071d57b5d2c123bb2b6272e8
mesum-abbas <messum321@gmail.com>
2025-03-24 06:14:46 +0500

    Update Day21.cpp

--- March/Day21.cpp
+++ March/Day21.cpp
@@ -31,3 +31,26 @@ public:
         return count;
     }
 };
+
+
+
+
+
+
+
+
+
+class Solution {
+public:
+    bool divideArray(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size();i+=2){
+            if(nums[i]!=nums[i+1]){
+                return false;
+            }
+        }
+        return true;
+    }
+            
+
+};
