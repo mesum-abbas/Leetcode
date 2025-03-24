87bb26d5e9ab4f8c81a346ea5fd39710dd5e76c4
mesum-abbas <messum321@gmail.com>
2025-03-24 06:14:09 +0500

    Update Day21.cpp

--- March/Day21.cpp
+++ March/Day21.cpp
@@ -13,3 +13,21 @@ public:
         return large;
     }
 };
+
+
+
+
+class Solution {
+public:
+    int diagonalSum(vector<vector<int>>& mat) {
+        int count = 0;
+        int n = mat.size();
+        for(int i=0;i<n;i++){
+            count+=mat[i][i];
+            if(i!=n-i-1){
+                     count+=mat[i][n-i-1];
+                }
+                }
+        return count;
+    }
+};
