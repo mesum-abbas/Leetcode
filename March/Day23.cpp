dd7c65e0a4a78c66c573184e2ba5547fe620d3ac
mesum-abbas <messum321@gmail.com>
2025-03-27 01:35:41 +0500

    Update Day23.cpp

--- March/Day23.cpp
+++ March/Day23.cpp
@@ -27,3 +27,27 @@ public:
         
     }
 };
+
+
+
+
+
+
+
+
+class Solution {
+public:
+bool isPrime(int n) {
+        return (n == 2 || n == 3 || n == 5 || n == 7 || n == 11 || n == 13 ||
+                n == 17 || n == 19 || n == 23 || n == 29 || n == 31);
+    }
+
+public:
+    int countPrimeSetBits(int left, int right) {
+        int cnt = 0;
+        for (int i = left; i <= right; i++)
+            if (isPrime(__builtin_popcount(i)))
+                cnt++;
+        return cnt;
+    }
+};
