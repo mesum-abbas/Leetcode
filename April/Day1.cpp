4a9b58721a1402e5bad9434dfdcbb5c2d92095d5
mesum-abbas <messum321@gmail.com>
2025-04-03 02:27:32 +0500

    Create Day1.cpp

---
+++ April/Day1.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int splitNum(int num) {
+        vector<int> digit;
+        while(num!=0){
+            digit.push_back(num%10);
+            num/=10;
+        }
+        sort(digit.begin(),digit.end());
+        int n = digit.size(); 
+        int num1=0;
+        int num2=0;
+        for(int i=0;i<n;i++){
+                if(i%2==0){
+                    num1 = (num1*10) + digit[i];
+                }
+                else{
+                    num2 = (num2 * 10) + digit[i];
+                }
+        }
+        return num2+num1;
+    }
+};
