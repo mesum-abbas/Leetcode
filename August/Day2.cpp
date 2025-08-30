13eec21f7f2dce67fceff2ff7566582d8f90ad64
mesum-abbas <messum321@gmail.com>
2025-08-30 15:12:00 +0500

    Create Day2.cpp

---
+++ August/Day2.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newinterval) {
+  vector<vector<int>> res ;
+  int i = 0 , n = intervals.size();
+
+  while( i<n && intervals[i][1] < newinterval[0]) {
+    res.push_back(intervals[i]);
+    i++;
+  }
+
+  while(i<n && intervals[i][0] <= newinterval[1]) {
+    newinterval[0] = min(newinterval[0] ,intervals[i][0]);
+     newinterval[1] = max(newinterval[1] , intervals[i][1]);
+     
+     i++;
+  }
+  res.push_back(newinterval);
+while(i<n) {
+    res.push_back(intervals[i]);
+    i++;
+  }
+  return res;
+
+
+
+    }
+};
